e2719c145602997d1f7f7e66368fb91ef8cefdbb
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-17 15:12:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'prints alphabet on lowercase'
+  *return: always 0
+  */
+
+int main(void)
+
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
