bef6ebc2442aec08273074a9998218852dfa7953
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-20 16:34:53 -0700

    tasks 0 to 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: "returns no value"
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	int n = 97;
+
+	n <= 122;
+	n++
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: "prints 10 times of the lowercase alphabets"
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 97; m <= 122; m++)
+		{
+			_putchar('m');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
