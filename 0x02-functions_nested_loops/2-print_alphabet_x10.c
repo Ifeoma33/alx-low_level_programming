7aa1a8d522bf438bb2cb04f0c88c482cb905c5a8
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-21 15:55:07 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,11 @@ void print_alphabet_x10(void)
 	char m;
 
 	for (n = 1; n <= 10; n++)
+	{
 		for (m = 'a'; m <= 'z'; m++)
 		{
-			_putchar('m');
+			_putchar(m);
 		}
 		_putchar('\n');
-
+	}
 }
