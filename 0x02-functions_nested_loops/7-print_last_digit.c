a16e197b5bb9d5f55eaf1cc01b3dc9ec857532ae
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-21 18:39:52 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: Prints last digit of an integer
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+	{
+		_putchar(-l + 0);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 0);
+		return (l);
+	}
+}
