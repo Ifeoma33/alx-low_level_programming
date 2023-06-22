01885ac48e31f32bcced1b3f3c53513e8de3d4f3
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-21 17:04:22 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: Checks for alphabets characters
+ *
+ * Return: Always 1 for success and 0 for failure
+ */
+int _isalpha(int c)
+{
+	if (c >= 'Aa' && c <= 'Zz')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
