6fb21bc2f6f58746d5ce8af7b6c7de5aab560896
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-24 15:13:04 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Multiply two numbers
+ * @a: Defines the first intigers
+ * @b: Deefines the second intigers
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int a = 40;
+	int b = 3;
+
+	mul(a, b);
+	_putchar(mul);
+	return (0);
+}
