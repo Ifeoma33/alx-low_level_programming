8c6a3dcbf7afc21a8aa59275afc5f943bd447422
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-06-25 15:00:43 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: Integers
+ * @b: Integers
+ * Description: Prints sum of two integers
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
