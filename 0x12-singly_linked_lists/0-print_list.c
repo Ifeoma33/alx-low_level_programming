5a2176e95a326a60c95709cdf1d15b4b2a41f970
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-07-27 16:08:09 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Prints all element of list_t
+ * list_t: List to be printed
+ * @h: Pointer to list_t
+ * Return: Number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t ab = 0;
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (ab);
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->set, h->str);
+
+		h = h->next;
+		ab++;
+	}
+	return (ab);
+}
