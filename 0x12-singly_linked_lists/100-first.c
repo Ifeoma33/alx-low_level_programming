8f55d9dac6b5beab44732ef00590bfb134cc89c6
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-07-27 20:00:20 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - Prints a sentence before the main
+ * Return: The expected output
+ *
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
