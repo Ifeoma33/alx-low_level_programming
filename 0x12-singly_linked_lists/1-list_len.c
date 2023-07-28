343bd7baf6f966f9935aee6a7fec451090d98b3b
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-07-27 18:39:18 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *
- *
- *
+ * list_len - Returns the numbers of element
+ * list_t: The element to be counted
+ * @h: Pointer to the list_t
+ * Return: Numbers of element
  */
+size_t list_len(const list_t *h)
+{
+	size_t bc = 0;
 
+	while (h != NULL)
+	{
+		bc++;
+		h = h->next;
+	}
+
+	return (bc);
+}
