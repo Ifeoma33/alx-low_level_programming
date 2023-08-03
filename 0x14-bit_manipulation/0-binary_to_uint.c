c66a59c4643fab954d943e4f5a116b7b2535d643
ifeoma33 <ifeomajanet.emeka@gmail.com>
2023-08-03 03:50:21 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to unsigned int
+ * @b: String containing the binary number
+ *
+ * Return: The converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int result = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (a = 0; b[a]; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+		{
+			return (0);
+		}
+		result = 2 * result + (b[a] - '0');
+	}
+
+	return (result);
+}
+
